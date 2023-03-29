58f85121197fe98f4f8e5242302bfc63eaac866a
Olga Ismagilova <75481045+oliismagilova@users.noreply.github.com>
2023-03-29 04:25:32 -0400

    Update ArrayMethods.js

--- ArrayMethods/ArrayMethods.js
+++ ArrayMethods/ArrayMethods.js
@@ -1,23 +1,23 @@
 Array.prototype.square = function square () {
-    return this.map (el => Math.pow (el, 2))
+     return this.map (el => typeof el == 'number' ? Math.pow (el, 2) : 0)
 }
 Array.prototype.cube = function cube () {
-    return this.map (el => el ** 3)
+    return this.map (el => typeof el == 'number' ? el**3 : 0)
 }
 Array.prototype.sum = function sum () {
-    return this.reduce ((x,y) => x+y, 0 )
+    return this.filter (el => typeof el == 'number' ? el : 0).reduce((x,y) => x+y, 0)
 }
 Array.prototype.average = function average () {
     return this.sum()/this.length;
 }
 Array.prototype.even = function even () {
     return this.filter (function(el){
-        if (el % 2 == 0) {
+        if (el % 2 == 0 && typeof el == 'number') {
             return el
         }
     })}
 Array.prototype.odd = function odd () {
-    return this.filter (el => el % 2 !==0)
+    return this.filter (el => typeof el == 'number' ? el % 2 !==0 : 0)
 }
 
 module.exports = Array.prototype.square;
