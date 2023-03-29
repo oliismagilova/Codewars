8c53c40dcc7925e65d0c0444bc101d86b86ae8f8
Olga Ismagilova <75481045+oliismagilova@users.noreply.github.com>
2023-03-29 04:26:07 -0400

    Update ArrayMethods.test.js

--- ArrayMethods/ArrayMethods.test.js
+++ ArrayMethods/ArrayMethods.test.js
@@ -7,32 +7,39 @@ const odd = require('./ArrayMethods');
 
 describe ('Array Methods', () => {
 var numbers = [1, 2, 3, 4, 5];
+var array = [1, 'three', 2,3,4]
 
 test('square() should return the square of each element', () => {
    expect(numbers.square()).toStrictEqual([ 1, 4, 9, 16, 25 ])
+    expect(array.square()).toStrictEqual([ 1, 0, 4, 9, 16 ])
 })
 
 test('cube() should return el ** 3', () => {
    expect(numbers.cube()).toStrictEqual([ 1, 8, 27, 64, 125 ])
+   expect(array.cube()).toStrictEqual([ 1, 0, 8, 27, 64 ]
+)
 })
 
 test('sum() should return the sum of all elements', () => {
    expect(numbers.sum()).toStrictEqual(15)
+   expect(array.sum()).toStrictEqual(10)
+
 })
 
 test('average() should return the average element among others', () => {
    expect(numbers.average()).toStrictEqual(3)
+   expect(array.average()).toStrictEqual(2)
 })
-
 test('even() should return the new array of even numebrs', () => {
    expect(numbers.even()).toStrictEqual([ 2, 4 ])
+   expect(array.even()).toStrictEqual([ 2, 4 ])
+   expect([1, 'three', 2,3, ' '].even()).toStrictEqual([ 2 ])
+
 })
 
 test('odd() should return the new array of odd numbers', () => {
    expect(numbers.odd()).toStrictEqual([ 1, 3, 5 ])
+   expect(array.odd()).toStrictEqual([ 1, 3 ])
+
 }) 
 })
-
-
-
-
